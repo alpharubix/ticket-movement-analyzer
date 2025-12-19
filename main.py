aa6a3ccb3b93581ba15aabdbc7e110ca9d46d37c
ashok <ashokmommyguy@gmail.com>
2025-12-19 17:57:00 +0530

    changed the entry file name to main for deploying

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ app = FastAPI(lifespan=lifespan)
 @app.get('/')
 def health_check():
     return {'message': 'server is up and running'}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 app.include_router(ticket_router)
 app.add_middleware(CORSMiddleware,allow_origins=["*"])
 if __name__ == "__main__":
